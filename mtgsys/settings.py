5985fcc106ad8f3934bf5a3d76bf43de1a9a5eda
j2100070 <j2100070@gunma-u.ac.jp>
2025-12-09 15:35:37 +0900

    log_admin削除

--- mtgsys/settings.py
+++ mtgsys/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'source.log_admin.LogAdminAccessMiddleware',
 ]
 
 # セッションエンジンをデータベースに設定
